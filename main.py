bfa0cfb5599e22cb7a3e87c3d482d85b6d3c17d2
Richard Franks <richard.franks@securenetcoms.co.uk>
2022-01-12 09:38:16 +0000

    Stuff

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from DolibarrApi import DolibarrApi
 from dotenv import load_dotenv
 from os import getenv
 import logging
+import datetime
+
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
+logging.getLogger("requests_oauthlib").setLevel(logging.ERROR)
 
 load_dotenv()
 
 s_api = SageApi(getenv('client_id'), getenv('client_secret'), getenv('redirect_uri'))
 d_api = DolibarrApi(getenv('dolibarr_url'), getenv('dolibarr_api_key'))
 
-lastrun="2000-01-01T00:00:00"
-lastrun="2021-10-28T10:00:00"
-
-logging.basicConfig(level=logging.DEBUG)
-logging.getLogger("urllib3").setLevel(logging.ERROR)
-logging.getLogger("requests_oauthlib").setLevel(logging.ERROR)
+with open("lastrun.txt", "r") as fh:
+    lastrun = fh.readline()
 
 from sync import sync_products, sync_services, sync_contacts, sync_purchase_invoices, sync_sales_invoices, sync_sales_quotes
 
@@ -24,3 +26,7 @@ sync_contacts(s_api, d_api, lastrun)
 sync_purchase_invoices(s_api, d_api, lastrun)
 sync_sales_invoices(s_api, d_api, lastrun)
 sync_sales_quotes(s_api, d_api, lastrun)
+
+lastrun = datetime.datetime.now().isoformat()[:-7]
+with open("lastrun.txt", "w") as fh:
+    lastrun = fh.write(lastrun)

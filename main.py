d3f4f21799cb563411ebee841ef18bfee295c922
Richard Franks <richard.franks@securenetcoms.co.uk>
2022-10-26 11:35:03 +0100

    Added Token and lastrun as environment variables

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ logging.getLogger("requests_oauthlib").setLevel(logging.ERROR)
 
 load_dotenv()
 
-s_api = SageApi(getenv('CLIENT_ID'), getenv('CLIENT_SECRET'), getenv('REDIRECT_URI'))
+s_api = SageApi(getenv('CLIENT_ID'), getenv('CLIENT_SECRET'), getenv('REDIRECT_URI'), token_file=getenv('TOKEN_FILE'))
 d_api = DolibarrApi(getenv('DOLIBARR_URL'), getenv('DOLIBARR_API_KEY'))
 
-with open("lastrun.txt", "r") as fh:
+with open(getenv('LASTRUN_FILE'), "r") as fh:
     lastrun = fh.readline()
 
 from sync import sync_products, sync_services, sync_contacts, sync_purchase_invoices, sync_sales_invoices, sync_sales_quotes
@@ -28,5 +28,5 @@ sync_sales_invoices(s_api, d_api, lastrun)
 sync_sales_quotes(s_api, d_api, lastrun)
 
 lastrun = datetime.datetime.now().isoformat()[:-7]
-with open("lastrun.txt", "w") as fh:
+with open(getenv('LASTRUN_FILE'), "w") as fh:
     lastrun = fh.write(lastrun)
